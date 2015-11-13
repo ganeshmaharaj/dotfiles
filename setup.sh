cf398699049894a716f013b352a2c0359727c055
Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
2015-11-13 15:45:37 +0000

    Flake8 Config file
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 dir=~/dotfiles
 dotfiles="gitconfig vimrc tmux.conf vim bash_aliases"
+declare -A config_files=( [flake8]="~/.config/" )
 
 cd $dir
 for file in $dotfiles
 do
 	rm -rf ~/.file
 	ln -s $dir/$file ~/.$file
+done
+
+for file in "${!config_files[@]}"
+do
+    mkdir -p ${config_files[$file]}
+    ln -s $dir/$file ${config_files[$file]}/${file}
 done
 
 echo "Setting up your bashrc.."
